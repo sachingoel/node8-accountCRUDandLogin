f8a582b6cdbee2236576dad30b0b6aef817b27cc
Sachin Goel <gsachingoel@gmail.com>
2018-07-23 19:39:45 +0530

    add user controller validation schema

---
+++ app/validationSchemas/userController.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let {celebrate,Joi} = require('celebrate');
+
+exports.registerUser = {
+  body:Joi.object().keys({
+    firstName   :Joi.string().min(3).max(15).required(),
+    lastName    :Joi.string().min(3).max(15),
+    mobileNumber:Joi.string().regex(/[0-9]{10}/).required(),
+    email       :Joi.string().email(/*{minDomainAtoms:2}*/).required(),
+    password    :Joi.string().alphanum().min(6).max(15).required(),
+    address     :Joi.object(),
+  })
+}
+
+exports.login = {
+	body:Joi.object().keys({
+		email				: Joi.string().email().required(),
+		mobileNumber: Joi.string().regex(/[0-9]{10}/),
+		password		: Joi.string().alphanum().min(6).max(15).required()
+	}).nand('email','mobileNumber')
+}
