ba3c7c97d3af4635e4f31d4475380ad403f1cd28
Sachin Goel <gsachingoel@gmail.com>
2018-07-23 19:06:45 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 // const config = require('./config/config.json');
 const {errors} = require('celebrate');
 
@@ -13,13 +14,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan('common'));
 
-// require('./routes')(app);
+require('./routes')(app);
 app.use(errors());
 
 
 app.listen(5454,function(err){
 	if(err){
-		console.log("unable to start the server due to error ", err);
-	}
+		console.log("Server stopped due to error", err);
+  }
+  mongoose.connect("mongodb://0.0.0.0/cClient");
 	console.log("-----cClient listening on port 5454-----");
 })
