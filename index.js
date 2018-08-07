33a625378d2ff13eea5bfa45d7aaec6bea895413
Sachin Goel <gsachingoel@gmail.com>
2018-08-07 09:38:21 +0530

    type(feat): add routes for social auth

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 'use Strict';
 const express = require("express");
+const cors = require('cors');
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require('passport');
 const {errors} = require('celebrate');
+const multer = require('multer');
+
+
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
+app.use(multer({}).array('Files',10))
 app.use(morgan('common'));
+app.use(passport.initialize());
+require('./app/middlewares/passport')(passport);
+
+
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+app.get('/auth/login/google',passport.authenticate('google',{scope:['email','profile']}));
+app.get('/auth/login/fb',passport.authenticate('facebook',{ scope: ['email']}));
+
+app.get('/auth/google/callback',passport.authenticate('google'),(req,res)=>{
+	res.json({token:res.locals.token});
+});
+
+app.get('/auth/facebook/callback',passport.authenticate('facebook'),(req,res)=>{
+	res.json({token:res.locals.token});
+});
 
 require('./routes')(app);
 app.use(errors());
